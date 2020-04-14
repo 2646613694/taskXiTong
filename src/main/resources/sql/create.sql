2ca49e5b7fb650b2e2b360ac73a9061b126b54b6
2646613694 <2646613694@qq.com>
2020-04-14 17:14:31 +0800

    提交数据库操作

---
+++ src/main/resources/sql/create.sql
@@ -0,0 +1,43 @@
+/*
+SQLyog v10.2 
+MySQL - 5.7.20-log : Database - task
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`task` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `task`;
+
+/*Table structure for table `sys_task` */
+
+DROP TABLE IF EXISTS `sys_task`;
+
+CREATE TABLE `sys_task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '任务ID',
+  `job_name` varchar(255) DEFAULT NULL COMMENT '任务名称',
+  `description` varchar(255) DEFAULT NULL COMMENT '任务描述',
+  `cron_expression` varchar(255) DEFAULT NULL COMMENT 'cron表达式',
+  `bean_class` varchar(255) DEFAULT NULL COMMENT '任务执行时调用哪个类的方法',
+  `job_status` varchar(255) DEFAULT NULL COMMENT '任务状态',
+  `job_group` varchar(255) DEFAULT NULL COMMENT '任务分组',
+  `create_user` varchar(255) DEFAULT NULL COMMENT '创建者',
+  `create_time` time DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(255) DEFAULT NULL COMMENT '更新者',
+  `update_time` time DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_task` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
